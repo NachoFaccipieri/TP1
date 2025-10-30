ed430b17160311332637eeb64fee91a9e6d220a1
NachoFaccipieri <ignacio.faccipieri@alu.ing.unlp.edu.ar>
2025-10-30 11:58:17 -0300

    Codigos para copilot

---
+++ Codigos/sx126x/out/Cohete/sx126x/src/sx126x.d
@@ -0,0 +1,3 @@
+Cohete/sx126x/out/Cohete/sx126x/src/sx126x.o: Cohete/sx126x/src/sx126x.c \
+ Cohete/sx126x/inc/sx126x.h Cohete/sx126x/inc/sx126x_status.h \
+ Cohete/sx126x/inc/sx126x_hal.h Cohete/sx126x/inc/sx126x_regs.h
